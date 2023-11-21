46631af2c8c902534d1757bc5fe9ecd7226de2ee
MiNiSoTaN <neill.gunderson@gmail.com>
2023-11-20 19:43:08 -0600

    changes on script.js

--- script.js
+++ script.js
@@ -22,14 +22,16 @@ function checkPasswordStrength() {
 function checkPasswordStrengthRules(password) {
     // Implement your password strength rules here
     var lengthRule = Math.min(password.length, 8); // Increment strength for each character beyond 8
-    var uppercaseRule = /[A-Z]/.test(password);
-    var lowercaseRule = /[a-z]/.test(password);
-    var digitRule = /\d/.test(password);
-    var specialCharRule = /[!@#$%^&*()-_=+[\]{}|;:'<>,.?/]/.test(password);
+    var uppercaseRule = /[A-Z]/.test(password) ? 1 : 0;
+    var lowercaseRule = /[a-z]/.test(password) ? 1 : 0;
+    var digitRule = /\d/.test(password) ? 1 : 0;
+    var specialCharRule = /[!@#$%^&*()-_=+[\]{}|;:'<>,.?/]/.test(password) ? 1 : 0;
 
     // Sum the rules to get a strength value between 0 and 5
     var strength = lengthRule + uppercaseRule + lowercaseRule + digitRule + specialCharRule;
 
-    return strength;
-}
+    // Calculate the percentage with a maximum of 100%
+    var strengthPercentage = Math.min(strength * 20, 100);
 
+    return strengthPercentage;
+}
