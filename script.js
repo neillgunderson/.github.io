bb14439072a4a5bd623dcfe22e1abd73b3444ea3
MiNiSoTaN <neill.gunderson@gmail.com>
2023-11-21 14:09:02 -0600

    Changed script.js file.

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ function checkPasswordStrength() {
     var strengthLabel = document.getElementById("strength-label");
 
     // Update the color of the strength bar based on the percentage
-    if (strengthPercentage < 20) {
+    if (strengthPercentage < 40) {
         strengthBar.style.backgroundColor = 'red';
-    } else if (strengthPercentage < 40) {
-        strengthBar.style.backgroundColor = 'orange';
     } else if (strengthPercentage < 60) {
-        strengthBar.style.backgroundColor = 'yellow';
+        strengthBar.style.backgroundColor = 'orange';
     } else if (strengthPercentage < 80) {
-        strengthBar.style.backgroundColor = 'green';
+        strengthBar.style.backgroundColor = 'yellow';
     } else {
         strengthBar.style.backgroundColor = '#00ff00';  // Bright green
     }
