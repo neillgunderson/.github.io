317de61c77ea8e5d31c1831b497f5ab2555f694d
MiNiSoTaN <neill.gunderson@gmail.com>
2023-11-21 13:59:43 -0600

    Changed script.js file.

--- script.js
+++ script.js
@@ -23,15 +23,14 @@ function checkPasswordStrength() {
 
 function calculatePasswordStrength(password) {
     // If there's at least one character and the length is less than 8, start with 20%
-    var initialStrength = password.length > 0 && password.length < 8 ? 10 : 0;
+    var initialStrength = password.length > 0 && password.length < 8 ? 20 : 0;
 
     // Increment strength for each character
     var strength = Math.min(password.length, 8);
 
     // Calculate the percentage with a maximum of 100%
-    var strengthPercentage = Math.min(initialStrength + strength * 0, 110);
+    var strengthPercentage = Math.min(initialStrength + strength * 10, 100);
 
     return strengthPercentage;
 }
-
 
